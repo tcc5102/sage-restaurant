794836e23dd54dcb9b95d1b9b7d80ca16834d778
Tyler Cooper <tyler.cooper91@gmail.com>
2017-01-31 15:20:39 -0500

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      # go to user show page upon sign in
+      root 'users#show', as: :authenticated_root
+    end
+
+    unauthenticated do
+      # login page if not signed in
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+
+  resources :users, only: [:show] do
+    resources :items, only: [:create, :destroy]
+  end
 end
